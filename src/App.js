c595ec6ab8b1d55275e6728f3b614b905d1803b2
Obnimariya <healthyandhappy.cuisine@gmail.com>
2022-09-07 12:21:52 +0300

    delete a by happy

--- src/App.js
+++ src/App.js
@@ -19,21 +19,6 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        ><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           Learn React
         </a>
